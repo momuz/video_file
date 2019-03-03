3b7fecc6604eb4a1eb8eb460987834315ff9dbdf
momuz <48169250+momuz@users.noreply.github.com>
2019-03-02 18:06:56 -1000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+const prefix = ("!");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
